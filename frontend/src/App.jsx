377dfabdbb67c8d212e3f11e7724dbd8b21b7f64
Abhi Mishra <sonusamda@gmail.com>
2024-09-28 13:28:25 +0530

    all routes fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Routes,Route, } from 'react-router-dom'
-import Home from './pages/Home'
+import {
+  Home,
+  Doctors,
+  Login,
+  MyAppointments,
+  Appointment,
+  Myprofile,
+  Contact,
+} from "../src/pages/index.js";
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className="mx-4 sm:mx-[10%]">
       <Routes>
-        
-       <Route path='/' element={<Home/>} />
-     </Routes>
-     
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-appointment" element={<MyAppointments />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/my-profile" element={<Myprofile />} />
+        <Route path="/contact" element={<Contact />} />
+       
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from "./Home.jsx";
+export { default as Doctors } from "./Doctors.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as MyAppointments } from "./MyAppointments.jsx";
+export { default as Appointment } from "./Appointment.jsx";
+export { default as Myprofile } from "./Myprofile.jsx";
+export { default as About } from "./About.jsx";
+export { default as Contact } from "./Contact.jsx";
